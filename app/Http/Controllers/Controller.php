e5cfdcb0281a7863832cb3532911734383727414
ElieOko <elieoko100@gmail.com>
2024-12-08 17:24:22 +0100

    feat:message and status request

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -5,4 +5,8 @@ namespace App\Http\Controllers;
 abstract class Controller
 {
     //
+    public $msg_success = "Enregistrement réussie avec succès";
+    public $msg_error = "Ressource non trouvé";
+    public $status_ok = 200;
+    public $status_error = 404;
 }
