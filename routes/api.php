98ddc040e5dc3e7691b570e381e7d1442ebd1333
ElieOko <elieoko100@gmail.com>
2024-12-08 17:33:32 +0100

    feat:api cities

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,10 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\CityController;
+
+Route::resource('cities', CityController::class)->only(['index', 'show','edit','store']);
 
 Route::middleware(['auth:sanctum'])->group(function () {
-
+    
 });
